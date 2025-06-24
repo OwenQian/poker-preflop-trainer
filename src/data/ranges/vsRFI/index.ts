479f91b14dd9317492c5737b1332301ee8d6190d
OwenQian <owenyqian@gmail.com>
2025-06-24 02:11:41 -0400

    delete nonsensical bb vs utg data

--- src/data/ranges/vsRFI/index.ts
+++ src/data/ranges/vsRFI/index.ts
@@ -6,8 +6,6 @@
  */
 
 // Import ranges
-import BB_vs_UTG_RFI from './BB-vs-UTG';
-import BB_vs_UTG2_RFI from './BB-vs-UTG2';
 import BB_vs_LJ_RFI from './BB-vs-LJ';
 import BB_vs_HJ_RFI from './BB-vs-HJ';
 import BB_vs_CO_RFI from './BB-vs-CO';
@@ -23,8 +21,6 @@ import BU_vs_CO_RFI from './BU-vs-CO';
 import CO_vs_HJ_RFI from './CO-vs-HJ';
 
 // Re-export ranges
-export { default as BB_vs_UTG_RFI } from './BB-vs-UTG';
-export { default as BB_vs_UTG2_RFI } from './BB-vs-UTG2';
 export { default as BB_vs_LJ_RFI } from './BB-vs-LJ';
 export { default as BB_vs_HJ_RFI } from './BB-vs-HJ';
 export { default as BB_vs_CO_RFI } from './BB-vs-CO';
@@ -41,8 +37,6 @@ export { default as CO_vs_HJ_RFI } from './CO-vs-HJ';
 
 // Collect all ranges into an array
 export const ALL_VS_RFI_RANGES = [
-  BB_vs_UTG_RFI,
-  BB_vs_UTG2_RFI,
   BB_vs_LJ_RFI,
   BB_vs_HJ_RFI,
   BB_vs_CO_RFI,
